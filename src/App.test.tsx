acb30c58f12dc6293dbcc56f00df611dc7dde0a0
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-11-18 00:29:55 +0000

    Support conversion and include test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,24 @@
 
 import { describe, test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
 describe('<App />', () => {
-  test('App mounts properly', () => {
-    const wrapper = render(<App />)
-    expect(wrapper).toBeTruthy()
+  test('correctly converts BRL per liter to USD per gallon', () => {
+    render(<App />)
+
+    // Populate price per liter in BRL
+    // Hard-coded conversion from BRL to USD: 1 USD = 5.7955874 BRL
+    // pricePerLiterInBRL * 3.78541 / 5.7955874 = pricePerGallonInUSD
+    // Example: 6.78 * 3.78541 / 5.7955874 = 4.42838284
+    // 4.42838284 rounded to 2 places is 4.43
+
+    // Expect price to show correctly in USD
+
+    const input = screen.getByRole('spinbutton')
+    fireEvent.change(input, {target: {value: '6.78'}})
 
     // Get by h1
-    const h1 = wrapper.container.querySelector('h1')
-    expect(h1?.textContent).toBe('Vite + React')
-
-    // Get by text using the React testing library
-    const text = screen.getByText(
-      /Click on the Vite and React logos to learn more/i
-    );
-    expect(text.textContent).toBeTruthy()
+    expect(screen.getAllByText(`4.43 USD per gallon`, { exact: false })).toBeTruthy()
   })
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const LITERS_PER_GALLON = 3.78541
 
+  // Updated on 2024-11-17
+  const BRL_PER_USD = 5.7955874
+
+  const [localPricePerLiter, setLocalPricePerLiter] = useState(0);
+  const getPriceInUSDFromPriceInBRL = (priceInBrl: number, brlPerUsd: number) => (priceInBrl / brlPerUsd).toFixed(2)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Convert BRL per liter to USD per gallon</h1>
+      R$ <input type="number" value={localPricePerLiter} onChange={(e) => setLocalPricePerLiter(Number(e.target.value))} /> BRL per liter
+      is ${getPriceInUSDFromPriceInBRL(localPricePerLiter * LITERS_PER_GALLON, BRL_PER_USD)} USD per gallon
     </>
   );
 }
