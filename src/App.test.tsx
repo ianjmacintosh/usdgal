6ec8e8e555fc711b08a3241af1ab4e48521c5e79
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-11-19 18:13:42 +0000

    Format files using Prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,10 @@
+import { describe, test, expect } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-import { describe, test, expect } from 'vitest'
-import { render, screen, fireEvent } from '@testing-library/react'
-import App from './App'
-
-describe('<App />', () => {
-  test('correctly converts BRL per liter to USD per gallon', () => {
-    render(<App />)
+describe("<App />", () => {
+  test("correctly converts BRL per liter to USD per gallon", () => {
+    render(<App />);
 
     // Populate price per liter in BRL
     // Hard-coded conversion from BRL to USD: 1 USD = 5.7955874 BRL
@@ -14,12 +13,12 @@ describe('<App />', () => {
     // 4.42838284 rounded to 2 places is 4.43
 
     // Expect price to show correctly in USD
-    const input = screen.getByLabelText('Local price (BRL per liter)')
-    fireEvent.change(input, {target: {value: '6.78'}})
+    const input = screen.getByLabelText("Local price (BRL per liter)");
+    fireEvent.change(input, { target: { value: "6.78" } });
 
     // Get output
-    const output = screen.getByLabelText('Home price (USD per gallon)')
+    const output = screen.getByLabelText("Home price (USD per gallon)");
     // Expect output to be 4.43
-    expect(output.value).toBe('4.43')
-  })
+    expect(output.value).toBe("4.43");
+  });
 });
