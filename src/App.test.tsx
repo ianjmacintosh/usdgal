dc6082ab726c807f2adcf19f723b387135c8d363
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-11-21 20:16:44 +0000

    Ensure backspacing works

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,6 +9,19 @@ describe("<App />", () => {
   const localPriceInput = screen.getByLabelText("Local price (BRL per liter)") as HTMLInputElement;
   const homePriceOutput = screen.getByLabelText("Home price (USD per gallon)") as HTMLInputElement;
 
+  test("allows the user to clear the input field", async () => {
+    // Populate price per liter in BRL
+    // Hard-coded conversion from BRL to USD: 1 USD = 5.7955874 BRL
+    // pricePerLiterInBRL * 3.78541 / 5.7955874 = pricePerGallonInUSD
+    // Example: 6.78 * 3.78541 / 5.7955874 = 4.42838284
+    // 4.42838284 rounded to 2 places is 4.43
+
+    // Expect price to show correctly in USD
+    await user.click(localPriceInput);
+    await user.keyboard('{backspace}{backspace}{backspace}{backspace}');
+    expect(localPriceInput.value).toBe("")
+  });
+
   test("correctly converts BRL per liter to USD per gallon", async () => {
     // Populate price per liter in BRL
     // Hard-coded conversion from BRL to USD: 1 USD = 5.7955874 BRL
@@ -18,7 +31,7 @@ describe("<App />", () => {
 
     // Expect price to show correctly in USD
     await user.click(localPriceInput);
-    await user.keyboard('{backspace}{backspace}{backspace}{backspace}6.78');
+    await user.keyboard('6.78');
     expect(localPriceInput.value).toBe("6.78")
 
     // Get output
