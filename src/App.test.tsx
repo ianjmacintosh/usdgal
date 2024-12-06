6acb32e95512ec8870842552cd9f9078c07358c5
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-12-06 21:07:57 +0000

    Remove unused import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { describe, test, expect, afterEach } from "vitest";
-import { cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
