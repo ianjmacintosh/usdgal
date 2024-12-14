0b059cde9622363a67897425f08820e2f8e37fe6
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-12-13 22:11:07 -0500

    🧪 test: Add tests for getGasPrice and App behavior (#64)
    * Add unit testing for getGasPrice
    
    * Add richer testing for updating correct price field
    
    Close #58

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -16,7 +16,7 @@ describe("<App />", () => {
   )[0] as HTMLSelectElement;
   const topUnitInput = screen.getAllByLabelText(
     "Unit of sale", { selector: 'select', exact: false },
-  )[0] as HTMLButtonElement;
+  )[0] as HTMLSelectElement;
   const bottomPriceInput = screen.getAllByLabelText(
     "Amount", { selector: 'input', exact: false },
   )[1] as HTMLInputElement;
@@ -97,37 +97,40 @@ describe("<App />", () => {
     expect(bottomPriceInput.value).toBe("1,234.00");
   });
 
-  test("updates bottom price (but not top price) when the user updates the target currency or units", async () => {
-    // Clear the local price input
-    await userEvent.clear(topPriceInput);
+  test("updates bottom price (but not top price) when the user updates the bottom currency or units", async () => {
+    // Perform the setup; 6.78, BRL, liters
+    await user.click(topPriceInput);
     await user.keyboard("6.78");
-
     await selectItemFromCombobox(topCurrencyButton, "Brazilian Real (BRL)")
     await user.selectOptions(topUnitInput, "per liter");
 
+    // Expect all the top values are as expected
     expect(topPriceInput.value).toBe("6.78");
+    expect(topCurrencyButton.textContent).toBe("BRL");
+    expect(topUnitInput.options[topUnitInput.selectedIndex].text).toBe("per liter");
+
+    // Expect the bottom value got converted right
     expect(bottomPriceInput.value).toBe(getFormattedPrice(getGasPrice(6.78, "BRL", "liter", "USD", "gallon"), "en-US", "USD"));
 
+    // Expect the top input to stay the same while updating the bottom currency
     await selectItemFromCombobox(bottomCurrencyButton, "Brazilian Real (BRL)")
+    expect(topPriceInput.value).toBe("6.78");
     expect(bottomPriceInput.value).toBe(getFormattedPrice(getGasPrice(6.78, "BRL", "liter", "BRL", "gallon"), "en-US", "BRL"));
 
+    // Expect the top input to stay the same while updating the bottom volume measure
     await user.selectOptions(bottomUnitInput, "per liter");
-    expect(bottomPriceInput.value).toBe(getFormattedPrice(getGasPrice(6.78, "BRL", "liter", "BRL", "liter"), "en-US", "USD"));
-  });
+    expect(bottomPriceInput.value).toBe(getFormattedPrice(getGasPrice(6.78, "BRL", "liter", "BRL", "liter"), "en-US", "BRL"));
 
-  test("updates the top price when the user changes the bottom price", async () => {
-    await selectItemFromCombobox(topCurrencyButton, /BRL/)
-    await user.selectOptions(topUnitInput, "per liter");
-    await selectItemFromCombobox(bottomCurrencyButton, /USD/)
-    await user.selectOptions(bottomUnitInput, "per gallon");
-
-    // Set a home price;
+    // Expect the top price to change when changing the bottom price
     await user.click(bottomPriceInput);
     await user.clear(bottomPriceInput);
     await user.keyboard("4.43");
     expect(bottomPriceInput.value).toBe("4.43");
+    expect(topPriceInput.value).toBe(getFormattedPrice(getGasPrice(4.43, "BRL", "liter", "BRL", "liter"), "en-US", "BRL"));
 
-    // Expect the local price field to have a value based on the home price
-    expect(topPriceInput.value).toBe(getFormattedPrice(getGasPrice(4.43, "USD", "gallon", "BRL", "liter"), "en-US", "USD"));
+    // Expect the top price to change when updating the bottom currency
+    await selectItemFromCombobox(bottomCurrencyButton, /USD/)
+    await user.selectOptions(bottomUnitInput, "per gallon")
+    expect(topPriceInput.value).toBe(getFormattedPrice(getGasPrice(4.43, "USD", "gallon", "BRL", "liter"), "en-US", "BRL"));
   });
 });
