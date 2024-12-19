b6c36b04c6bb323d047087ec85a59f49a87360cb
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-12-19 20:43:45 +0000

    Style volumes to look right

--- src/Unit.tsx
+++ src/Unit.tsx
@@ -1,25 +1,49 @@
 import * as Ariakit from "@ariakit/react";
-import "./Currency.css"
-
+import "./Unit.css";
 
 type SupportedUnits = "liter" | "gallon";
-const Unit = (({ id, unit, onUnitChange, disabled }: {
-    id: string,
-    unit: string,
-    onUnitChange: (newValue: SupportedUnits) => void,
-    disabled?: boolean
+const Unit = ({
+    id,
+    unit,
+    onUnitChange,
+    disabled,
+}: {
+    id: string;
+    unit: string;
+    onUnitChange: (newValue: SupportedUnits) => void;
+    disabled?: boolean;
 }) => {
     return (
-        <>
-            <Ariakit.SelectProvider defaultValue={unit} setValue={(newValue: SupportedUnits) => { onUnitChange(newValue) }} id={id}>
-                <Ariakit.Select className="button" aria-label="Unit of sale (liters or gallons)" disabled={disabled}>{unit === "liter" ? "per liter" : "per gallon"}</Ariakit.Select>
-                <Ariakit.SelectPopover gutter={4} sameWidth className="popover" unmountOnHide={true}>
-                    <Ariakit.SelectItem className="select-item" value="gallon">per gallon</Ariakit.SelectItem>
-                    <Ariakit.SelectItem className="select-item" value="liter">per liter</Ariakit.SelectItem>
-                </Ariakit.SelectPopover>
-            </Ariakit.SelectProvider>
-        </>
-    )
-});
+        <Ariakit.SelectProvider
+            defaultValue={unit}
+            setValue={(newValue: SupportedUnits) => {
+                onUnitChange(newValue);
+            }}
+            id={id}
+        >
+            <Ariakit.Select
+                className="unit-button button"
+                aria-label="Unit of sale (liters or gallons)"
+                disabled={disabled}
+            >
+                <span className="current-value">{unit === "liter" ? "per liter" : "per gallon"}</span>
+                <Ariakit.SelectArrow className="chevron" />
+            </Ariakit.Select>
+            <Ariakit.SelectPopover
+                gutter={4}
+                sameWidth
+                className="popover unit-popover"
+                unmountOnHide={true}
+            >
+                <Ariakit.SelectItem className="select-item" value="gallon">
+                    {unit === "gallon" ? "✓" : ""} per gallon
+                </Ariakit.SelectItem>
+                <Ariakit.SelectItem className="select-item" value="liter">
+                    {unit === "liter" ? "✓" : ""} per liter
+                </Ariakit.SelectItem>
+            </Ariakit.SelectPopover>
+        </Ariakit.SelectProvider>
+    );
+};
 
 export default Unit;
