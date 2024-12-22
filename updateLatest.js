59121e634c8df88ca64b845a0d9de93efc392d6e
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-12-22 14:35:36 +0000

    Get back to a working point

--- updateLatest.js
+++ updateLatest.js
@@ -2,9 +2,6 @@ import { request } from "node:https";
 import * as fs from "fs";
 
 const API_KEY = process.env.EXCHANGERATESAPI_KEY;
-const SAVED_DATA_PATH = "./src/latest.json";
-const API_HOSTNAME = "www.ianjmacintosh.com";
-const API_PATH = "/index.html";
 
 if (typeof API_KEY === "undefined") {
   console.error(
@@ -13,43 +10,27 @@ if (typeof API_KEY === "undefined") {
   process.exit(1);
 }
 
-const isExchangeRateDataOld = true;
-
-// if (!isExchangeRateDataOld) {
-//   console.log(
-//     `No need to update the exchange rate data, no changes made to saved data (${SAVED_DATA_PATH})`,
-//   );
-//   process.exit(0);
-// }
-
 const options = {
-  hostname: API_HOSTNAME,
+  hostname: "www.ianjmacintosh.com",
   port: 443,
-  path: API_PATH,
+  path: `/}`,
   method: "GET",
 };
 
 const req = request(options, (res) => {
-    res.on("data", (chunk) => {
-      rawData += chunk;
-    });
-    res.on("end", () => {
-      if (JSON.parse(rawData)?.success !== true) {
-        console.err(
-          `Endpoint did not respond with an explicit success, no changes made to saved data (${SAVED_DATA_PATH})`,
-        );
-        req.end();
-        process.exit(1);
-      }
-      try {
-        fs.writeFile(SAVED_DATA_PATH, rawData, (err) => {
-          console.error(err);
-        });
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  }
+  let rawData = "";
+  res.on("data", (chunk) => {
+    rawData += chunk;
+  });
+  res.on("end", () => {
+    try {
+      fs.writeFile("./src/ijm-index.html", rawData, (err) => {
+        console.error(err);
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
 });
 
 req.on("error", (e) => {
@@ -57,4 +38,3 @@ req.on("error", (e) => {
 });
 
 req.end();
-process.exit(0);
