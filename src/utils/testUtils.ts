f0be3dcda6ae8c840462e4396e41bb4adeb028e2
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-12-24 21:34:32 +0000

    Format everything

--- src/utils/testUtils.ts
+++ src/utils/testUtils.ts
@@ -3,17 +3,17 @@ import userEvent from "@testing-library/user-event";
 import { expect } from "vitest";
 
 const selectItemFromFancySelect = async (
-    selectElement: Element,
-    option: string,
-  ) => {
-    const user = userEvent.setup();
-    await user.click(selectElement);
-    const popover = document.querySelector(".popover") as HTMLElement;
-    await user.click(getByText(popover, option, { exact: false }));
+  selectElement: Element,
+  option: string,
+) => {
+  const user = userEvent.setup();
+  await user.click(selectElement);
+  const popover = document.querySelector(".popover") as HTMLElement;
+  await user.click(getByText(popover, option, { exact: false }));
 
-    waitFor(() => {
-      expect(selectElement.textContent).toBe(option);
-    });
-  };
+  waitFor(() => {
+    expect(selectElement.textContent).toBe(option);
+  });
+};
 
-  export { selectItemFromFancySelect }
+export { selectItemFromFancySelect };
