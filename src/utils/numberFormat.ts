46ee76995b993afc87e1cfe7d2ea50aa15fc3ced
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-12-25 18:32:58 +0000

    Refactor isLegalPriceValue to support other languages

--- src/utils/numberFormat.ts
+++ src/utils/numberFormat.ts
@@ -13,27 +13,30 @@ const getNumberFormatChar = (
   return chars[char];
 };
 
-const isTinyNumber = (number: number, userLocale = "en-US", currency = "USD") => {
+const isTinyNumber = (
+  number: number,
+  userLocale = "en-US",
+  currency = "USD",
+) => {
   if (number === 0 || number >= 1) {
-    return false
+    return false;
   }
 
-  const formattedNumber = 
-  Intl.NumberFormat(userLocale, {
+  const formattedNumber = Intl.NumberFormat(userLocale, {
     style: "currency",
     currency: currency,
     currencyDisplay: "code",
   })
-  .format(number)
-  .replace(currency, "")
-  .trim()
+    .format(number)
+    .replace(currency, "")
+    .trim();
 
   if (Number(formattedNumber) === 0) {
-    return true
+    return true;
   }
 
-  return false
-}
+  return false;
+};
 
 const getFormattedPrice = (
   price: number,
@@ -41,15 +44,15 @@ const getFormattedPrice = (
   currency = "USD",
 ) => {
   let formattedNumber = String(price);
-  
+
   formattedNumber = Intl.NumberFormat(userLocale, {
     style: "currency",
     currency: currency,
     currencyDisplay: "code",
   })
-  .format(price)
-  .replace(currency, "")
-  .trim();
+    .format(price)
+    .replace(currency, "")
+    .trim();
 
   // If we're formatting the number to look like 0 but the value isn't 0,
   //  replace the last 0 with a 1
@@ -57,28 +60,36 @@ const getFormattedPrice = (
     formattedNumber = formattedNumber.replace(/0$/, "1");
   }
 
-  return formattedNumber
+  return formattedNumber;
 };
 
-const isLegalPriceValue = (price: string) => {
-  // Generate a regular expression that confirms a character is legal for en-US formatting
-  const isLegalPriceChar = new RegExp(/[0-9\\.\\,]/);
+const isLegalPriceValue = (price: string, userLocale = "en-US") => {
+  const decimalSeparatorChar = getNumberFormatChar(
+    "decimalSeparatorChar",
+    userLocale,
+  );
+  const groupingSeparatorChar = getNumberFormatChar(
+    "groupingSeparatorChar",
+    userLocale,
+  );
 
-  // Is this something that someone logically type if they were writing a number out one character at a time?
-  // RegExp should allow values:
-  // - Any number of digits (including after the decimal point)
-  // - Any number of commas
-  // - Could start or end with a decimal point
-  // - Optionally, one decimal point
-  // - No commas allowed after the decimal point
-  const isLegalPrice = new RegExp(/^(\d{0,}(,\d{0,})*|\d*)?(\.\d*)?$/);
+  // Generate a regular expression that confirms a character is legal for systems of writing that use commas and periods
+  const isLegalPriceChar = new RegExp(/[0-9\\.\\,]/);
 
   const newChar = price?.slice(-1);
   // If the new value is not "" and the new char is not a number, return
   if (price && isLegalPriceChar.test(newChar) === false) return false;
 
-  // If the new value is not a number, return
-  if (isLegalPrice.test(price) === false) return false;
+  // Is this something that someone logically type if they were writing a number out one character at a time?
+  // Only one decimal separator is allowed
+  if (
+    price.split(decimalSeparatorChar).length > 2 ||
+    // No grouping separators allowed after the decimal separator
+    price.split(decimalSeparatorChar)[1]?.split(groupingSeparatorChar).length >
+      1
+  ) {
+    return false;
+  }
 
   return true;
 };
@@ -102,4 +113,10 @@ const getUnits = (
   return price;
 };
 
-export { getUnits, getNumberFormatChar, getFormattedPrice, isLegalPriceValue, isTinyNumber };
+export {
+  getUnits,
+  getNumberFormatChar,
+  getFormattedPrice,
+  isLegalPriceValue,
+  isTinyNumber,
+};
