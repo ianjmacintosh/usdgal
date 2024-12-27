626ae69e4d3e1ea99aa4aad3fde54c4272952120
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-12-27 02:22:53 +0000

    Get common elements from an object

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,6 +12,23 @@ const TestComponent = ({ ...props }) => {
 
 describe("<App />", () => {
   const user = userEvent.setup();
+  const elements = () => {
+    return {
+      topPriceInput: screen.getAllByLabelText(/Amount/)[0],
+      bottomPriceInput: screen.getAllByLabelText(
+        /Amount/,
+      )[1] as HTMLInputElement,
+      topCurrencyInput: screen.getAllByLabelText("Currency")[0],
+      topUnitInput: screen.getAllByLabelText("Unit of sale", {
+        exact: false,
+      })[0],
+      bottomCurrencyInput: screen.getAllByLabelText("Currency")[1],
+      bottomUnitInput: screen.getAllByLabelText("Unit of sale", {
+        exact: false,
+      })[1],
+    };
+  };
+
   beforeEach(() => {
     render(<TestComponent />);
   });
@@ -20,26 +37,14 @@ describe("<App />", () => {
   });
   test("can convert a gas price from one currency to another", async () => {
     // Arrange
-    const topPriceInput = screen.getAllByLabelText(
-      /Amount/,
-    )[0] as HTMLInputElement;
-
-    const bottomPriceInput = screen.getAllByLabelText(
-      /Amount/,
-    )[1] as HTMLInputElement;
-
-    const topCurrencyInput = screen.getAllByLabelText(
-      "Currency",
-    )[0] as HTMLButtonElement;
-    const topUnitInput = screen.getAllByLabelText("Unit of sale", {
-      exact: false,
-    })[0] as HTMLButtonElement;
-    const bottomCurrencyInput = screen.getAllByLabelText(
-      "Currency",
-    )[1] as HTMLButtonElement;
-    const bottomunitInput = screen.getAllByLabelText("Unit of sale", {
-      exact: false,
-    })[1] as HTMLButtonElement;
+    const {
+      topPriceInput,
+      bottomPriceInput,
+      topCurrencyInput,
+      topUnitInput,
+      bottomCurrencyInput,
+      bottomUnitInput,
+    } = elements();
 
     const convertedPrice = getGasPrice(1, "BRL", "liter", "USD", "gallon");
     const formattedPrice = getFormattedPrice(convertedPrice, "en-US", "BRL");
@@ -48,7 +53,7 @@ describe("<App />", () => {
     await selectItemFromFancySelect(topCurrencyInput, "BRL");
     await selectItemFromFancySelect(topUnitInput, "per liter");
     await selectItemFromFancySelect(bottomCurrencyInput, "USD");
-    await selectItemFromFancySelect(bottomunitInput, "per gallon");
+    await selectItemFromFancySelect(bottomUnitInput, "per gallon");
     await user.click(topPriceInput);
     await user.keyboard("1");
 
