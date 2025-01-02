310b85499dd726b3f8f7b54175087ebfe908d280
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-02 22:41:43 +0000

    Fix routing setup

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,10 @@ import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
   route("/", "./pages/home-en.tsx"),
-  route("/de*?", "./pages/home-de.tsx"),
-  route("/es*?", "./pages/home-es.tsx"),
-  route("/pt*?", "./pages/home-pt.tsx"),
-  route("/hi*?", "./pages/home-hi.tsx"),
-
-  // * matches all URLs, the ? makes it optional so it will match / as well
-  route("*", "./pages/home-en.tsx"),
+  route("/de", "./pages/home-de.tsx"),
+  route("/es", "./pages/home-es.tsx"),
+  route("/pt", "./pages/home-pt.tsx"),
+  route("/hi", "./pages/home-hi.tsx"),
+  // * matches all URLs
+  route("/*", "./catchall.tsx"),
 ] satisfies RouteConfig;
