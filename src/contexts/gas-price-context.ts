4f945122a05ad76879818a451f54d9ffc9318545
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-08 16:08:17 +0000

    Remove unused type

--- src/contexts/gas-price-context.ts
+++ src/contexts/gas-price-context.ts
@@ -3,15 +3,6 @@ import { Units } from "@/components/unit/unit";
 import getGasPrice from "@/utils/get-gas-price";
 import { createContext } from "react";
 
-export type GasPriceContext = {
-  number: number;
-  setNumber: (number: number) => void;
-  currency: string;
-  setCurrency: (currency: string) => void;
-  unit: Units;
-  setUnit: (unit: Units) => void;
-} | null;
-
 type GasPriceAction = {
   type: "update" | "setDriver";
   id: string;
