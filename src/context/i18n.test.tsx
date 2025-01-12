4dffe1cd81a6c22ffa271eec6bab17e88a62b310
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-12 17:55:12 +0000

    Add starter test file

---
+++ src/context/i18n.test.tsx
@@ -0,0 +1,23 @@
+import { describe, test, expect, beforeEach } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { I18nProvider } from "./i18n";
+
+const TestComponent = ({ ...props }: { [key: string]: unknown }) => {
+  return (
+    <I18nProvider {...props}>
+      <h1>Hello World</h1>
+    </I18nProvider>
+  );
+};
+
+beforeEach(() => {
+  cleanup();
+  render(<TestComponent />);
+});
+
+describe("<I18nProvider />", () => {
+  test("renders child elements", async () => {
+    expect(screen.getByText("Hello World")).toBeVisible();
+  });
+});
