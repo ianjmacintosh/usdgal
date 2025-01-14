d7ad34f4b53164f7ef2b93ead984046c14a903d8
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-14 20:58:15 +0000

    Update fetch logic to support dynamic CF Pages URLs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,22 @@
 const API_GETLOCATION_PATH = "/workers/getLocation";
 
 export async function fetchCountryCode() {
-  // Create URL based on environment variables; worst case, go to prod
-  const serverUrl =
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5173"
-      : "https://gasco.st";
-  const geolocationApiUrl = new URL(API_GETLOCATION_PATH, serverUrl);
+  // Create absolute URL; the concept of a relative fetch does not make sense in Node.js
+
+  // Default to Production
+  let baseUrl = "https://gasco.st";
+
+  // Cloudflare Pages preview deploys use CF_PAGES_URL
+  if (typeof process !== "undefined" && process.env.CF_PAGES_URL) {
+    baseUrl = process.env.CF_PAGES_URL;
+
+    // Development deploys use localhost:5173
+  } else if (import.meta.env.DEV) {
+    baseUrl = "http://localhost:5173";
+  }
+
+  const geolocationApiUrl = new URL(API_GETLOCATION_PATH, baseUrl);
+
   try {
     const response = await fetch(geolocationApiUrl);
     const data = await response.json();
