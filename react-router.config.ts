3e3b013105cf74ea0fa5665b04eb0a073c24c0b8
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-14 19:18:02 +0000

    Explicitly disable SSR

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   appDirectory: "src",
+  ssr: false,
   async prerender() {
     return ["/", "/de", "/es", "/pt", "/hi"];
   },
