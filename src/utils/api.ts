6b7a02cb571302b825c3d78f74cbc24b824c492e
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-14 21:45:33 +0000

    Expose CF_PAGES_URL variable for location lookup

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,17 +4,25 @@ export async function fetchCountryCode() {
   // Create absolute URL; the concept of a relative fetch does not make sense in Node.js
 
   // Default to Production
-  let baseUrl = "https://gasco.st";
+  let baseUrl;
 
-  // Cloudflare Pages preview deploys use CF_PAGES_URL
-  if (typeof process !== "undefined" && process.env.CF_PAGES_URL) {
-    baseUrl = process.env.CF_PAGES_URL;
+  switch (import.meta.env.MODE) {
+    case "production":
+      if (typeof import.meta.env.CF_PAGES_URL === "undefined") {
+        throw new Error(
+          "While building for Production, could not find Cloudflare Pages URL",
+        );
+      }
 
-    // Development deploys use localhost:5173
-  } else if (import.meta.env.DEV) {
-    baseUrl = "http://localhost:5173";
+      baseUrl =
+        import.meta.env.CF_PAGES_BRANCH === "main"
+          ? "https://gasco.st"
+          : import.meta.env.CF_PAGES_URL;
+      break;
+    case "development":
+      baseUrl = "http://localhost:5173";
+      break;
   }
-
   const geolocationApiUrl = new URL(API_GETLOCATION_PATH, baseUrl);
 
   try {
