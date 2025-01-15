3d2097c08e1f6702c15819a486f0ea7d16dba93d
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-15 17:22:06 -0500

    🧭 ci: Make API requests to relevant environment (#160)
    * Send all requests to production by default
    
    Regardless of environment or mode, geolocation requests go to production
    unless VITE_SERVER_URL is set
    
    * Remove unused environment prefices
    
    * Assume developers will use localhost 5173
    
    * Use CF_PAGES_URL for server URL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,25 +4,14 @@ export async function fetchCountryCode() {
   // Create absolute URL; the concept of a relative fetch does not make sense in Node.js
 
   // Default to Production
-  let baseUrl;
+  let baseUrl = "https://gasco.st";
 
-  switch (import.meta.env.MODE) {
-    // Production should connect to https://gasco.st/
-    case "production":
-      baseUrl = "https://gasco.st";
-      break;
-
-    // Preview builds should connect to their Cloudflare Pages URL (exposed via vite.config.ts "envPrefix" setting)
-    case "preview":
-      baseUrl = import.meta.env.CF_PAGES_URL;
-      break;
-
-    // Everything else should connect to non-HTTPS localhost: http://localhost:5173
-    case "testing":
-    case "development":
-    default:
-      baseUrl = "http://localhost:5173";
-      break;
+  // Cloudflare Pages exposes the HTTP server's host as "CF_PAGES_URL"
+  if (import.meta.env.CF_PAGES_URL) {
+    // In Production, my build settings set CF_PAGES_URL=https://gasco.st
+    // In Preview environments, Cloudflare dynamically sets CF_PAGES_URL
+    // In Development, ".env.development" sets CF_PAGES_URL=http://localhost:5173
+    baseUrl = import.meta.env.CF_PAGES_URL;
   }
 
   const geolocationApiUrl = new URL(API_GETLOCATION_PATH, baseUrl);
