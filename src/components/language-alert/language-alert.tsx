a556a3621e2e242e11a0f64c9a4ed8f675082d5a
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-16 21:29:12 +0000

    Add close button and headline to alert

--- src/components/language-alert/language-alert.tsx
+++ src/components/language-alert/language-alert.tsx
@@ -10,15 +10,30 @@ const LanguageAlert = () => {
 
   return (
     displayMessage && (
-      <div className="language-alert">
-        <p>
-          This site is in Spanish, but your system preferences are set to
-          English.
-        </p>
-        <p>
-          <a href="/">Go to the American English site</a>
-        </p>
-      </div>
+      <aside className="language-alert" role="alert">
+        <h2>English Site Available</h2>
+        <button type="button" aria-label="Close" className="close-button">
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 44 44"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <path d="M0.549989 4.44999L4.44999 0.549988L43.45 39.55L39.55 43.45L0.549989 4.44999Z" />
+            <path d="M39.55 0.549988L43.45 4.44999L4.44999 43.45L0.549988 39.55L39.55 0.549988Z" />
+          </svg>
+        </button>
+        <main>
+          <p>
+            This site is in Spanish, but your system preferences are set to
+            English.
+          </p>
+          <p>
+            <a href="/">Go to the American English site</a>
+          </p>
+        </main>
+      </aside>
     )
   );
 };
