64c2793432da5a320ecf3c8db92502f86521eab5
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-17 17:23:32 +0000

    Add localStorage hook

---
+++ src/utils/use-local-storage.ts
@@ -0,0 +1,27 @@
+// Stolen from Ibadehin Mojeed's article "Using localStorage with React Hooks"
+// https://blog.logrocket.com/using-localstorage-react-hooks/
+
+import { useState, useEffect } from "react";
+
+function getStorageValue(key: string, defaultValue: unknown) {
+  if (typeof window === "undefined" || typeof localStorage === "undefined") {
+    return defaultValue;
+  }
+  // getting stored value
+  const saved = localStorage.getItem(key) || "{}";
+  const initial = JSON.parse(saved);
+  return initial || defaultValue;
+}
+
+export const useLocalStorage = (key: string, defaultValue: unknown) => {
+  const [value, setValue] = useState(() => {
+    return getStorageValue(key, defaultValue);
+  });
+
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
