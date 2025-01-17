2f84f416ecd00cb5e09d143325ba296660660f9e
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-01-17 17:07:31 +0000

    Add language handling tests

---
+++ e2e-tests/language-handling.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from "@playwright/test";
+
+test.use({
+  locale: "en-US",
+});
+
+test.describe("An en-US user", () => {
+  test.describe("visiting the default (English) homepage", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("/");
+    });
+
+    test("sees an English heading", async ({ page }) => {
+      await expect(
+        page.getByRole("heading", { name: "Gas Cost", exact: true }),
+      ).toBeVisible();
+    });
+
+    test("can use the language select to see the site in German", async ({
+      page,
+    }) => {
+      await page.getByRole("combobox", { name: "Language" }).click();
+      await page.getByRole("option", { name: "Deutsch" }).click();
+
+      await expect(
+        page.getByRole("heading", { name: "Gaskosten", exact: true }),
+      ).toBeVisible();
+
+      await expect(page.getByRole("combobox", { name: "Sprache" })).toHaveText(
+        "Deutsch",
+      );
+    });
+  });
+
+  test.describe("visiting the /de/ (German) homepage", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("/de/");
+    });
+
+    test("does not see an English heading", async ({ page }) => {
+      await expect(
+        page.getByRole("heading", { name: "Gas Cost", exact: true }),
+      ).not.toBeVisible();
+    });
+
+    test("sees a language alert", async ({ page }) => {
+      await expect(page.getByRole("alert")).toBeVisible();
+    });
+
+    test("can dismiss the language alert", async ({ page }) => {
+      await page.getByRole("button", { name: "Close" }).click();
+      await expect(page.getByRole("alert")).not.toBeVisible();
+    });
+
+    test.skip("doesn't see the language alert again if they dismiss it and reload", async ({
+      page,
+    }) => {
+      await page.reload();
+      await expect(page.getByRole("alert")).not.toBeVisible();
+    });
+  });
+});
