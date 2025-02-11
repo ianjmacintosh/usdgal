012303ee3c62c4602a31da0f799d4cc20bf2163d
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-02-11 19:00:26 +0000

    Return 404 response for missing pages

---
+++ e2e-tests/404.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+const nonexistentPages = ["/$", "/t"];
+
+for (const url of nonexistentPages) {
+  test.describe(`${url} response`, () => {
+    test("throws a 404 error", async ({ request }) => {
+      const response = await request.get(url);
+
+      expect(response.status()).toBe(404);
+    });
+  });
+}
