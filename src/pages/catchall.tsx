8acb30f9865c13f2fe53795d76a1e227956dde21
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-03-21 19:10:03 +0000

    Return 404 status code on missing routes
    This renders an ugly 404 page with the correct status code

--- src/pages/catchall.tsx
+++ src/pages/catchall.tsx
@@ -1,3 +1,38 @@
+import {
+  defaultLinks,
+  getMetaTags,
+} from "../utils/remix-page-attribute-helpers.ts";
+import { data } from "react-router";
+import { getMessage } from "@/context/i18n.tsx";
+
+const language = "en";
+
+export const links = () => {
+  return [
+    ...defaultLinks(),
+    {
+      rel: "canonical",
+      href: getMessage({ id: "meta_canonical_url", language }),
+    },
+  ];
+};
+
+export function meta() {
+  // Create meta tags with no title
+  const metaTags = getMetaTags(language).filter(
+    (node) => !Object.keys(node).includes("title"),
+  );
+
+  // Add a title
+  metaTags.push({ title: "404 Not Found" });
+
+  return metaTags;
+}
+
 export async function loader() {
-  throw new Response(null, { status: 404, statusText: "Not Found" });
+  return data({ message: "Not Found" }, { status: 404 });
+}
+
+export default function Component() {
+  return <p>404 Not Found</p>;
 }
