5f0e24afba95587af3e1351f15d64d6703ce6a6d
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-10-07 18:14:31 +0000

    Add link and icon
    Icon courtesy of SVG Box Heroicons-Outline

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,8 @@
 import LanguageSelect from "@/components/language-select/language-select";
+import "./footer.css";
 import { FormattedMessage } from "react-intl";
 import GithubLogo from "@/assets/github.svg?react";
+import DocumentText from "@/assets/document-text.svg?react";
 
 type FooterProps = {
   siteLanguage: string;
@@ -12,6 +14,22 @@ const Footer = ({ siteLanguage }: FooterProps) => {
       <LanguageSelect siteLanguage={siteLanguage} />
       <nav>
         <ul>
+          {/* Link to article about building this app */}
+          <li>
+            <a
+              href="https://www.ianjmacintosh.com/articles/introducing-gascost/"
+              target="_blank"
+            >
+              <span>
+                <FormattedMessage
+                  id="article"
+                  defaultMessage={"About this site"}
+                />
+              </span>
+              <DocumentText height={18} width={18} className="link-icon" />
+            </a>
+          </li>
+          {/* Link to source code */}
           <li>
             <a
               href="https://www.github.com/ianjmacintosh/usdgal"
@@ -25,10 +43,12 @@ const Footer = ({ siteLanguage }: FooterProps) => {
           </li>
         </ul>
       </nav>
-      &copy; 2025{" "}
-      <a href="https://www.ianjmacintosh.com/" target="_blank">
-        Ian J. MacIntosh
-      </a>
+      <p>
+        &copy; 2025{" "}
+        <a href="https://www.ianjmacintosh.com/" target="_blank">
+          Ian J. MacIntosh
+        </a>
+      </p>
     </footer>
   );
 };
