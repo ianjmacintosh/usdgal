c85fc21222a9b2d638f33d59520c0ed274b95e74
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-10-11 02:40:32 +0000

    Manage language alert via state

--- src/components/language-alert/language-alert.tsx
+++ src/components/language-alert/language-alert.tsx
@@ -1,5 +1,5 @@
 import "./language-alert.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import { I18nProvider, useI18n } from "@/context/i18n";
 import { getClosestSupportedLanguage } from "@/utils/supported-languages";
@@ -39,8 +39,13 @@ const LanguageAlert = () => {
   // Only offer a translation if the site language is both...
   // - Not one the user has suggested they want by dismissing the alert
   // - Not our idea of the best language to show them based on their system settings and our supported languages
-  const wrongLanguage = siteLanguage !== preferredLanguageId;
+  const [wrongLanguage, setWrongLanguage] = useState(
+    siteLanguage !== preferredLanguageId,
+  );
 
+  useEffect(() => {
+    setWrongLanguage(siteLanguage !== preferredLanguageId);
+  }, [siteLanguage, preferredLanguageId]);
   const closeMessage = () => {
     // Update the user's preferred language; if they want to see the site in the language they're seeing it in, leave them alone
     setPreferredLanguageId(siteLanguage);
@@ -53,44 +58,43 @@ const LanguageAlert = () => {
   };
 
   return (
-    wrongLanguage && (
-      <I18nProvider siteLanguage={preferredLanguageId}>
-        <div
-          className="language-alert"
-          role="alert"
-          aria-labelledby="language-alert-label"
+    <I18nProvider siteLanguage={preferredLanguageId}>
+      <div
+        className="language-alert"
+        role="alert"
+        aria-labelledby="language-alert-label"
+        aria-hidden={!wrongLanguage}
+      >
+        <figure className="translate-icon">
+          <TranslateIcon height={20} width={20} />
+        </figure>
+        <main>
+          <a href={preferredLanguagePath}>
+            <p id="language-alert-label">
+              <FormattedMessage id="languageAlertText" />
+            </p>
+          </a>
+        </main>
+        <button
+          type="button"
+          aria-label="Close"
+          className="close-button"
+          onClick={closeMessage}
+          ref={closeButtonElement}
         >
-          <figure className="translate-icon">
-            <TranslateIcon height={20} width={20} />
-          </figure>
-          <main>
-            <a href={preferredLanguagePath}>
-              <p id="language-alert-label">
-                <FormattedMessage id="languageAlertText" />
-              </p>
-            </a>
-          </main>
-          <button
-            type="button"
-            aria-label="Close"
-            className="close-button"
-            onClick={closeMessage}
-            ref={closeButtonElement}
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 44 44"
+            aria-hidden="true"
+            focusable="false"
           >
-            <svg
-              width="15"
-              height="15"
-              viewBox="0 0 44 44"
-              aria-hidden="true"
-              focusable="false"
-            >
-              <path d="M0.549989 4.44999L4.44999 0.549988L43.45 39.55L39.55 43.45L0.549989 4.44999Z" />
-              <path d="M39.55 0.549988L43.45 4.44999L4.44999 43.45L0.549988 39.55L39.55 0.549988Z" />
-            </svg>
-          </button>
-        </div>
-      </I18nProvider>
-    )
+            <path d="M0.549989 4.44999L4.44999 0.549988L43.45 39.55L39.55 43.45L0.549989 4.44999Z" />
+            <path d="M39.55 0.549988L43.45 4.44999L4.44999 43.45L0.549988 39.55L39.55 0.549988Z" />
+          </svg>
+        </button>
+      </div>
+    </I18nProvider>
   );
 };
 
