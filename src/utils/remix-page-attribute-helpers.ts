c2a8a536d10ac83da0c9121845699a502f019940
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-10-14 18:36:30 +0000

    Add web app manifest

--- src/utils/remix-page-attribute-helpers.ts
+++ src/utils/remix-page-attribute-helpers.ts
@@ -10,12 +10,14 @@ export const defaultLinks = () => {
   // * Each page uses absolute URLs
   // (Source: [Portent blog](https://portent.com/blog/seo/implement-hreflang-canonical-tags-correctly.htm))
   return [
+    { rel: "icon", type: "image/svg+xml", href: "/gas_pump.svg" },
     { rel: "alternate", hrefLang: "en", href: "https://gasco.st/" },
     { rel: "alternate", hrefLang: "de", href: "https://gasco.st/de/" },
     { rel: "alternate", hrefLang: "pt", href: "https://gasco.st/pt/" },
     { rel: "alternate", hrefLang: "es", href: "https://gasco.st/es/" },
     { rel: "alternate", hrefLang: "hi", href: "https://gasco.st/hi/" },
     { rel: "alternate", hrefLang: "x-default", href: "https://gasco.st/" },
+    { rel: "manifest", href: "/gascost.webmanifest" },
   ];
 };
 
