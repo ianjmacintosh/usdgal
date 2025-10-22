150e3d5e1dcf97fb598e4372becd82a02e6af765
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-10-22 22:57:26 +0000

    Run E2E tests against Cloudflare-like setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173",
+    baseURL: "http://localhost:8788",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -62,8 +62,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
-    url: "http://localhost:5173",
+    command: "npm run dev:wrangler",
+    url: "http://localhost:8788",
     reuseExistingServer: !process.env.CI,
   },
 });
