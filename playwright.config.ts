f08ae8d55802a28c31a67ac8ba3ebcbb99986ee2
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-11-25 18:25:03 +0000

    Run E2E tests with Wrangler

--- playwright.config.ts
+++ playwright.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev:wrangler",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
