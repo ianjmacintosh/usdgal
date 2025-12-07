3ebf198d0f60e56fe88a786459d933ef6609c599
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-12-07 16:24:36 -0300

    🏗️  feat: Handle 404s correctly on Vite (#234)
    * :bug: fix: Fix infinite loop when getlocation is inaccessible (#232)
    
    * Update npm dependencies
    
    * Configure Wrangler
    
    * Run E2E tests with Wrangler
    
    * Geolocate users to US by default
    
    * Use Cloudflare with Vite
    
    * Disable lazy route discovery
    
    * Explicitly tell SPA to handle Not Found requests
    
    * Skip 404 tests for now
    
    * Update npm dependencies
    
    * Use Wrangler by default
    
    * Revert "Update npm dependencies"
    
    This reverts commit 470c6ae7fc8593d40eb54cbb310898d7c5c43537.
    
    * Handle missing language as any other not found
    
    * Update Wrangler to use 404 page
    
    * Skip the 404 tests (404's don't work on prod)
    
    * Revert "Update Wrangler to use 404 page"
    
    This reverts commit a15068b71a1018e15e6308c7288330efe9ece056.

--- e2e-tests/404.spec.ts
+++ e2e-tests/404.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-const nonexistentPages = ["/$", "/t"];
+const nonexistentPages = [
+  "/$", // Wacky symbol
+  "/t", // I've got production requests this
+  "/foo", // Not a language code at all
+  "/foo/", // Not a language code... and this time it's a directory!
+  "/test/", // For some reason, 4-char strings launch a different error than 3-char strings
+  "/xx/", // Not a language code but could be because it's 2 chars
+  "/it/", // A language code that's not supported
+  "/de/foo/", // A language code that's supported, but we're looking for a resource that doesn't exist
+];
 
 for (const url of nonexistentPages) {
   test.describe.skip(`${url} response`, () => {
